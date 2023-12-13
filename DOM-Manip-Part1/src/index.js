9e41f93055d1df8061a38580d190224bf4955efb
Soumaia <94118305+Soumaiald@users.noreply.github.com>
2023-12-13 15:38:46 +0100

    adding part 2 to  index.js

--- DOM-Manip-Part1/src/index.js
+++ DOM-Manip-Part1/src/index.js
@@ -15,14 +15,57 @@ topMenuEl.className = "flex-around";
 console.log(topMenuEl.outerHTML);
 var menuLinks = [
   { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
+/****** Previous menuLinks *********/
+/*[
+  { text: "about", href: "/about" },
   { text: "catalog", href: "/catalog" },
   { text: "orders", href: "/orders" },
   { text: "account", href: "/account" },
-];
+];*/
 
 for (let l of menuLinks) {
   const ach = document.createElement("a");
   ach.href = l.href;
   ach.textContent = l.text;
   topMenuEl.appendChild(ach);
+}
+/******************** DOM Part 2 ***********************/
+const subMenuEl = document.querySelector("#sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor =
+  getComputedStyle(root).getPropertyValue("--sub-menu-bg");
+subMenuEl.className = "flex-around";
+console.log(subMenuEl.outerHTML);
+subMenuEl.style.position = "relative";
+subMenuEl.style.top = "0";
+subMenuEl.addEventListener("click", handle);
+function handle() {
+  console.log("handle function was called");
 }
